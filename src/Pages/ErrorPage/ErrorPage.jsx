43e54a1c092ab970bf99b4e199036439540c2f85
Shahin Alom <shahinalomshuvo94@gmail.com>
2023-11-24 12:47:25 +0600

    create signIn form

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,32 +1,38 @@
 import Lottie from "lottie-react";
 import { Link, useRouteError } from "react-router-dom";
 import errorAnimation from "../../assets/AnimationFile/error.json";
+import { Helmet } from "react-helmet";
 
 const ErrorPage = () => {
   const error = useRouteError();
   console.error(error);
   return (
-    <div className='flex flex-col justify-center items-center h-screen text-center bg-[#B8E0EB] bg-opacity-20'>
-      <div className='w-full'>
-        <Lottie
-          className='max-w-full h-96'
-          animationData={errorAnimation}
-          loop={true}
-        />
+    <>
+      <Helmet>
+        <title>404 error</title>
+      </Helmet>
+      <div className='flex flex-col justify-center items-center h-screen text-center bg-[#B8E0EB] bg-opacity-20'>
+        <div className='w-full'>
+          <Lottie
+            className='max-w-full h-96'
+            animationData={errorAnimation}
+            loop={true}
+          />
+        </div>
+        <div id='error-page'>
+          <h1 className='text-xl text-red-600 font-semibold'>Oops!</h1>
+          <p className='text-xl '>Sorry, an unexpected error has occurred.</p>
+          <p className='text-bold text-2xl'>
+            <i>
+              {error.status} {error.statusText || error.message}
+            </i>
+          </p>
+          <Link to={"/"}>
+            <button className='btn btn-info mt-2'>Go Home</button>
+          </Link>
+        </div>
       </div>
-      <div id='error-page'>
-        <h1 className='text-xl text-red-600 font-semibold'>Oops!</h1>
-        <p className='text-xl '>Sorry, an unexpected error has occurred.</p>
-        <p className='text-bold text-2xl'>
-          <i>
-            {error.status} {error.statusText || error.message}
-          </i>
-        </p>
-        <Link to={"/"}>
-          <button className='btn btn-info mt-2'>Go Home</button>
-        </Link>
-      </div>
-    </div>
+    </>
   );
 };
 
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,11 @@
+import { Helmet } from "react-helmet";
+
 const Home = () => {
   return (
     <div>
+      <Helmet>
+        <title>ContestHub | Home</title>
+      </Helmet>
       <h2>This is home</h2>
     </div>
   );
