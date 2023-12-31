e189f07cbb83614c9df827c2d57fe661aa16fc68
Shahin Alom <shahinalomshuvo94@gmail.com>
2023-12-31 16:51:03 +0600

    add myTeam

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -8,6 +8,7 @@ import SearchResult from "./SearchResult";
 import HowToSelect from "../HowToSelect/HowToSelect";
 import HeroSection from "./HeroSection";
 import TrustedBy from "./TrustedBy";
+import MyTeam from "./MyTeam";
 
 const Home = () => {
   const [searchValue, setSearchValue] = useState("");
@@ -22,6 +23,7 @@ const Home = () => {
       <HeroSection setSearchValue={setSearchValue}></HeroSection>
       {searchValue && <SearchResult value={searchValue}></SearchResult>}
       <PopularContest></PopularContest>
+      <MyTeam></MyTeam>
       <ContestWinner></ContestWinner>
       <TrustedBy></TrustedBy>
       <HowToSelect></HowToSelect>
---
+++ src/Pages/Home/MyTeam.jsx
@@ -0,0 +1,35 @@
+import bgImg from "../../assets/Images/bg-2.jpg";
+import feature from "../../assets/Images/features.webp";
+import { Parallax } from "react-parallax";
+
+const MyTeam = () => {
+  return (
+    <div>
+      <Parallax
+        blur={{ min: -15, max: 15 }}
+        bgImage={bgImg}
+        bgImageAlt='Parallax img'
+        strength={-200}
+      >
+        <div className='container mx-auto grid md:grid-cols-3 gap-10 py-10 px-4 lg:px-0'>
+          <div className='text-white md:col-span-2 flex flex-col justify-center space-y-4'>
+            <h2 className='text-4xl font-bold'>
+              Be loved by the players in your league.
+            </h2>
+            <p>
+              Create and customize teams and players. Get access to many
+              personalized statistics for socces, basketball, cricket, League of
+              Leagends, Call of Duty, Fortnite, Counter Strike and many other
+              sports and games. Find out who is the best!
+            </p>
+          </div>
+          <div>
+            <img className='w-full' src={feature} alt='' />
+          </div>
+        </div>
+      </Parallax>
+    </div>
+  );
+};
+
+export default MyTeam;
